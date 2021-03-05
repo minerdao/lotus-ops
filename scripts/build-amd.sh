21e21ed798203105efa7fdac18bf220f13adfe7d
wenhui.yu <winfreyu@gmail.com>
2021-03-05 22:08:05 +0800

    fix: build test

--- scripts/build-amd.sh
+++ scripts/build-amd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-env RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 make clean all
+env RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 make clean all lotus-bench lotus-shed
 sudo make install
 
 lotus -v
--- scripts/build-intel.sh
+++ scripts/build-intel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-env CGO_CFLAGS_ALLOW="-D__BLST_PORTABLE__" RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 CGO_CFLAGS="-D__BLST_PORTABLE__" make clean all
+env CGO_CFLAGS_ALLOW="-D__BLST_PORTABLE__" RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 CGO_CFLAGS="-D__BLST_PORTABLE__" make clean all lotus-bench lotus-shed
 sudo make install
 
 lotus -v
