714ece8d1272644ec4a0c1933a323dbc398b7cd0
wenhui.yu <winfreyu@gmail.com>
2021-03-17 22:24:22 +0800

    feat: base worker

---
+++ scripts/hostnameC2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt install net-tools -y
+IP=`ifconfig |grep broadcast |awk '{print $2}'`
+IP01=`ifconfig |grep broadcast |awk '{print $2}'|awk -F. '{print $1}'`
+IP02=`ifconfig |grep broadcast |awk '{print $2}'|awk -F. '{print $2}'`
+IP03=`ifconfig |grep broadcast |awk '{print $2}'|awk -F. '{print $3}'`
+IP04=`ifconfig |grep broadcast |awk '{print $2}'|awk -F. '{print $4}'`
+
+echo "WorkerC-$IP01-$IP02-$IP03-$IP04" >/etc/hostname
+echo "WorkerC-$IP01-$IP02-$IP03-$IP04" >>/etc/hosts
+
