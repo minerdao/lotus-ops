c8833a48a49625d761d751855c80f05d93f809c4
wenhui.yu <winfreyu@gmail.com>
2021-04-05 21:43:15 +0800

    feat: setup daemon

--- scripts/setup-daemon.sh
+++ scripts/setup-daemon.sh
@@ -44,21 +44,21 @@ tee /etc/netplan/50-cloud-init.yaml <<'EOF'
 network:
   version: 2
   ethernets:
-    enp198s0f0:
+    enp197s0f0:
       dhcp4: true
       dhcp6: true
-    enp198s0f1:
+    enp197s0f1:
       dhcp4: true
       dhcp6: true
-    enp194s0f0:
+    enp193s0f0:
       dhcp4: no
-    enp194s0f1:
+    enp193s0f0:
       dhcp4: no
     bonds:
       bond0:
         interfaces:
-          - enp194s0f0
-          - enp194s0f1
+          - enp193s0f0
+          - enp193s0f1
         addresses: [ipAddress/24]
         gateway4: 10.0.99.1
         nameservers:
