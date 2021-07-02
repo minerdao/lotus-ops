9e3fe00d1fc40b35f9702cb603a74aaf1d58daa1
wenhui.yu <winfreyu@gmail.com>
2021-07-02 09:49:25 +0800

    fix: disabled gpu2

--- scripts/build-amd.sh
+++ scripts/build-amd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-env RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 FFI_USE_GPU2=1 make clean all lotus-bench lotus-shed
+env RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 FFI_USE_GPU2=0 make clean all lotus-bench lotus-shed
 
 sudo make install
 
