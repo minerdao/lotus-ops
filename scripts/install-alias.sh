1c5c4ea8a46f42bfd461d12173e03e98722d74d4
wenhui.yu <winfreyu@gmail.com>
2021-10-15 16:17:25 +0800

    fix: post miner tmpdir

---
+++ scripts/install-alias.sh
@@ -0,0 +1,11 @@
+echo "# lotus alias" >> ~/.profile
+echo "alias lsm=/usr/local/bin/lotus-miner" >> ~/.profile
+echo "alias info=/home/fil/workspace/lotus-ops/alias/info.sh" >> ~/.profile
+echo "alias jobs=/home/fil/workspace/lotus-ops/alias/jobs.sh" >> ~/.profile
+echo "alias workers=/home/fil/workspace/lotus-ops/alias/workers.sh" >> ~/.profile
+echo "alias sectors=/home/fil/workspace/lotus-ops/alias/sectors.sh" >> ~/.profile
+echo "alias status=/home/fil/workspace/lotus-ops/alias/status.sh" >> ~/.profile
+echo "alias abort=/home/fil/workspace/lotus-ops/alias/abort.sh" >> ~/.profile
+echo "alias remove=/home/fil/workspace/lotus-ops/alias/remove.sh" >> ~/.profile
+
+source ~/.profile
--- scripts/install-ansible.sh
+++ scripts/install-ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
