52a08ba28bab9bdad7c860c3576a5ebaa1a5924a
Max <max@coinsummer.io>
2022-07-02 12:17:58 +0800

    feat: build cudaa

---
+++ scripts/build-cuda.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+env RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 FFI_USE_CUDA=1 make clean all lotus-bench lotus-shed
+
+sudo make install
+
+lotus -v
+lotus-miner -v
