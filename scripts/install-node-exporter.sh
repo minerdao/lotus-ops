ec12d02fd2a7ce80d264043a8cc1dd033ddf73da
Max <max@coinsummer.io>
2022-09-29 10:48:39 +0800

    fix: node-exporter url update

--- scripts/install-node-exporter.sh
+++ scripts/install-node-exporter.sh
@@ -3,12 +3,12 @@ currentUser=fil
 workspace=/home/$currentUser/workspace
 
 cd $workspace
-wget -c https://cs-cn-filecoin.oss-cn-beijing.aliyuncs.com/node-exporter/node_exporter-0.18.1.linux-amd64.tar.gz
-tar xfvz node_exporter-0.18.1.linux-amd64.tar.gz
+wget -c https://github.com/prometheus/node_exporter/releases/download/v1.4.0/node_exporter-1.4.0.linux-amd64.tar.gz
+tar xfvz node_exporter-1.4.0.linux-amd64.tar.gz
 
-sudo mv node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
+sudo mv node_exporter-1.4.0.linux-amd64/node_exporter /usr/local/bin/
 sudo chmod a+x /usr/local/bin/node_exporter
-rm -rf $workspace/node_exporter-0.18.1.linux-amd64*
+rm -rf $workspace/node_exporter-1.4.0.linux-amd64*
 
 mkdir -p /home/$currentUser/prometheus/run
 
